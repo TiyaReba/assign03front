8ac5703aa3988bb4b9b71d45b47e4fd119e69d61
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2022-09-28 14:27:21 +0530

    change in seever.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const path = require("path");
-const app = express();
+const app = new express();
 
 
 app.use(express.static("./dist/frontend"));
